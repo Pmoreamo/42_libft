209e9234b1377e9314688f4cbf5c02641d85a492
Pau <pau.anand.morello@gmail.com>
2026-05-14 04:40:45 +0200

    Milestone-0

---
+++ ft_lstmap.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmorello <pmorello@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 12:24:53 by pmorello          #+#    #+#             */
+/*   Updated: 2024/04/25 08:28:17 by pmorello         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	void	*temp;
+	t_list	*new;
+	t_list	*elem;
+
+	if (!lst)
+		return (NULL);
+	new = NULL;
+	while (lst)
+	{
+		temp = f(lst->content);
+		elem = ft_lstnew(temp);
+		if (!elem)
+		{
+			del(temp);
+			ft_lstclear(&new, del);
+		}
+		ft_lstadd_back(&new, elem);
+		lst = lst-> next;
+	}
+	return (new);
+}
